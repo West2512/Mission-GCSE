3dfed23a9b47c07dad3f34be13b6b4a9c7bf13d9
West2512 <biancacrowhurst@hotmail.co.uk>
2026-07-22 21:16:59 +0100

    adding Storage

--- app.js
+++ app.js
@@ -2230,63 +2230,6 @@ const elements = {
 };
 
 /* =========================================================
-   STORAGE
-   ========================================================= */
-
-function loadState() {
-  try {
-    const storedState = localStorage.getItem(STORAGE_KEY);
-
-    if (!storedState) {
-      return createDefaultState();
-    }
-
-    const parsedState = JSON.parse(storedState);
-
-    return {
-      ...createDefaultState(),
-      ...parsedState,
-      completedMissions:
-        Array.isArray(parsedState.completedMissions)
-          ? parsedState.completedMissions
-          : [],
-      completedActivities:
-        Array.isArray(parsedState.completedActivities)
-          ? parsedState.completedActivities
-          : [],
-      activeDates:
-        Array.isArray(parsedState.activeDates)
-          ? parsedState.activeDates
-          : [],
-      unlockedAchievements:
-        Array.isArray(parsedState.unlockedAchievements)
-          ? parsedState.unlockedAchievements
-          : [],
-      activityScores:
-        parsedState.activityScores &&
-        typeof parsedState.activityScores === "object"
-          ? parsedState.activityScores
-          : {}
-    };
-  } catch (error) {
-    console.error("Could not load Mission GCSE progress.", error);
-
-    return createDefaultState();
-  }
-}
-
-function saveState() {
-  try {
-    localStorage.setItem(
-      STORAGE_KEY,
-      JSON.stringify(appState)
-    );
-  } catch (error) {
-    console.error("Could not save Mission GCSE progress.", error);
-  }
-}
-
-/* =========================================================
    GENERAL HELPERS
    ========================================================= */
 
